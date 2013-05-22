cbc4f3520fce293f783591098207a9b0757a907c
Ann Kpx <annkpx@gmail.com>
2013-05-22 17:39:50 +0400

    comments

--- dominanta/views.py
+++ dominanta/views.py
@@ -3,13 +3,14 @@
 import datetime
 from django.core.context_processors import csrf
 from django.contrib import messages
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.http import Http404, HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from livesettings import config_value
 
 from pages.models import Page
-from blog.models import Article, Category, ArticleTag
+from blog.models import Article, Category, ArticleTag, Comment
 
 def get_common_context(request):
     c = {}
@@ -48,19 +49,43 @@ def articles_page(request, category=None):
         c['breadcrumb'].reverse()
         c['breadcrumb'] = c['breadcrumb'][:-1]
         
-        c['articles'] = Article.objects.filter(category__in=categories)
-        return render_to_response('articles.html', c, context_instance=RequestContext(request))
+        items = Article.objects.filter(category__in=categories)
     else:
-        c['articles'] = Article.objects.all()
-        return render_to_response('articles.html', c, context_instance=RequestContext(request))
+        items = Article.objects.all()
+    
+    paginator = Paginator(items, 2)
+    page = int(request.GET.get('page', '1'))
+    c['get_request'] = c['request_url'][:-1]
+    try:
+        c['articles'] = paginator.page(page)
+    except PageNotAnInteger:
+        page = 1
+        c['articles'] = paginator.page(page)
+    except EmptyPage:
+        page = paginator.num_pages
+        c['articles'] = paginator.page(page)
+    c['page'] = page
+    c['page_range'] = paginator.page_range
+    if len(c['page_range']) > 1:
+        c['need_pagination'] = True
+    return render_to_response('articles.html', c, context_instance=RequestContext(request))
     
 def article_page(request, category, name):
+    
+    
+    
     c = get_common_context(request)
     
     c['category'] = Category.get_by_slug(category)
     descendants = c['category'].get_descendants(include_self=True)
     
     c['article'] = Article.objects.get(slug=name, category__in=descendants)
+    if request.method == 'POST':
+        if request.POST['text']:
+            Comment(article=c['article'],
+                    name=request.user.first_name + ' ' + request.user.last_name,
+                    text=request.POST['text']).save()
+        return HttpResponseRedirect(request.path)
     categories = c['article'].category.all()
     curr_cat = None
     for cat in categories:
@@ -70,7 +95,6 @@ def article_page(request, category, name):
     
     c['breadcrumb'] = []
     while curr_cat:
-        print curr_cat
         c['breadcrumb'].append(curr_cat)
         curr_cat = curr_cat.parent
     c['breadcrumb'].reverse()
