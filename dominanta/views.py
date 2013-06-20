ee20d90beb356c5da8324df59c9be2bca8cf8bac
Ann Kpx <annkpx@gmail.com>
2013-06-20 23:50:34 +0400

    панель справа

--- dominanta/views.py
+++ dominanta/views.py
@@ -181,12 +181,16 @@ def archive_filetype_page(request, id):
     c = get_common_context(request)
     c['filetype'] = FileType.objects.get(id=id)
     c['files'] =  ArchiveFile.objects.filter(filetype=id)
+    c['medspec'] = Specialty.objects.get(slug='medspetsialnosti').get_children().filter(show=True).order_by('order')
+    c['dokl'] = Specialty.objects.get(slug='doklinika').get_children().filter(show=True).order_by('order')
     return render_to_response('archive_filetype.html', c, context_instance=RequestContext(request))
  
 def archive_category_page(request, id):
     c = get_common_context(request)
     c['category'] = Specialty.objects.get(id=id)
     c['files'] =  ArchiveFile.objects.filter(category=id)
+    c['medspec'] = Specialty.objects.get(slug='medspetsialnosti').get_children().filter(show=True).order_by('order')
+    c['dokl'] = Specialty.objects.get(slug='doklinika').get_children().filter(show=True).order_by('order')
     return render_to_response('archive_category.html', c, context_instance=RequestContext(request))
  
 """
